01d4c6da651e62f6b5008ebe80b09fbd37948557
Artur Safin <rusgamehack@gmail.com>
2022-10-15 17:43:56 +0300

    DimasterSyte

---
+++ Dima/js/main.js
@@ -0,0 +1,5 @@
+let hang = document.querySelector('.hang-bg-1'),
+    hang2 = document.querySelector('.hang-bg-2');
+window.addEventListener("scroll",()=>{
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+});
