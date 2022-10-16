7f81014eb3fb2354059f152270cc4673ae1342db
Artur Safin <rusgamehack@gmail.com>
2022-10-16 22:16:49 +0300

    fixGithub

--- Dima/js/music.js
+++ Dima/js/music.js
@@ -1,7 +1,7 @@
-const music = [new Audio('../music/1.mp3'), new Audio('../music/2.mp3'), 
-                new Audio('../music/3.mp3'), new Audio('../music/4.mp3'), 
-                new Audio('../music/5.mp3'), new Audio('../music/6.mp3'), 
-                new Audio('../music/7.mp3'), new Audio('../music/8.mp3')];
+const music = [new Audio('./music/1.mp3'), new Audio('./music/2.mp3'), 
+                new Audio('./music/3.mp3'), new Audio('./music/4.mp3'), 
+                new Audio('./music/5.mp3'), new Audio('./music/6.mp3'), 
+                new Audio('./music/7.mp3'), new Audio('./music/8.mp3')];
 music.volume = 1;
 let ch = 0;
 document.querySelector('.hang-img').addEventListener('click', function(){
