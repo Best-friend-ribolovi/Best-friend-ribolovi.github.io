e0e87fce6266fcf2190ecceb00367e2266fc6d2f
Artur Safin <rusgamehack@gmail.com>
2022-10-17 20:29:18 +0300

    NewFixMusic

--- Dima/js/music.js
+++ Dima/js/music.js
@@ -1,14 +1,14 @@
-const music = [new Audio('./music/1.mp3'), new Audio('./music/2.mp3'), 
-                new Audio('./music/3.mp3'), new Audio('./music/4.mp3'), 
-                new Audio('./music/5.mp3'), new Audio('./music/6.mp3'), 
-                new Audio('./music/7.mp3'), new Audio('./music/8.mp3')];
+const music = [new Audio('../music/1.mp3'), new Audio('../music/2.mp3'), 
+                new Audio('../music/3.mp3'), new Audio('../music/4.mp3'), 
+                new Audio('../music/5.mp3'), new Audio('../music/6.mp3'), 
+                new Audio('../music/7.mp3'), new Audio('../music/8.mp3')];
 music.volume = 1;
 let ch = 0;
-document.querySelector('.hang-img').addEventListener('click', function(){
-    music[ch].currentTime = 0;
-    music[ch].play();
-    ch++;
-    if(ch>7){
-        ch=0;
-    }
+document.querySelector('.hang-img-photos').addEventListener('click', function(e){
+    target = e.target;
+    console.log(target);
+    let c = target.classList[1].replace('music','') - 1;
+    console.log(c);
+    music[c].currentTime = 0;
+    music[c].play();
 })
