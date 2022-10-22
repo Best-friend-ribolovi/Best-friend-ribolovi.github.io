76141e6ab3cdbf06fd2589b3574c732dc0ffa14c
Artur Safin <rusgamehack@gmail.com>
2022-10-22 14:25:55 +0300

    Dima

--- Dima/index.php
+++ Dima/index.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Волшебство музыки с Дмитрием Садовым</title>
+    <link rel="icon" href="favicon.ico"> <!-- 32×32 -->
+    <link rel="icon" href="images/favicons/icon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="images/favicons/apple.png">  <!-- 180×180 -->
+    <link rel="manifest" href="manifest.webmanifest">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif&family=PT+Sans&display=swap" rel="stylesheet">
@@ -13,11 +17,11 @@
 <body>
     <header id="header">
         <div class="container">
-            <menu>
+            <div class="menu">
                 <ul>
                     <li><a href="#me" class="yakor">обо мне</a></li><li><a href="#gift" class="yakor">услуги</a></li><li><a href="#contacts" class="yakor">контакты</a></li>
                 </ul>
-            </menu>
+            </div>
             <h1 class="title">ПО-НАСТОЯЩЕМУ ВОЛШЕБНОЕ ЗВУЧАНИЕ УНИКАЛЬНОГО ИНСТРУМЕНТА</h1>
             <div class="descr">музыка ханга</div>
             <div class="header-links">
@@ -27,7 +31,7 @@
     </header>
     <section class="hang" id="hang">
         <div class="container">
-            <div class="title">ХАНГ - ОДИН ИЗ НОВЕЙШИХ<br>АКУСТИЧЕСКИХ ИНСТРУМЕНТОВ</div>
+            <h2 class="title">ХАНГ - ОДИН ИЗ НОВЕЙШИХ<br>АКУСТИЧЕСКИХ ИНСТРУМЕНТОВ</h2>
         </div>
         <div class="hang-block">
             <div class="text">Ханг (хендпан) является <br>одним из самых молодых акустических<br>инструментов, его придумала фирма PanArt в 2000 году. <br>Звучание этого инструмента невозможно спутать ни с чем, физически это гибрид ксилофона<br>и ручного барабана.</div>
@@ -43,10 +47,9 @@
     </section>
     <section class="me" id="me">
         <div class="container">
-            <div class="title">обо мне</div>
-            <div class="text">
-                Меня зовут Дмитрий, я играю на ханге. <br>Этот инструмент обладает уникальным звучанием, <br>не похожим ни на что иное и способным заполнить<br>большое пространство.</div>
-            <a target="_ blank" href="https://youtu.be/G9_pUmt-nww" class="link-button">смотреть видео</a>
+            <h2 class="title">обо мне</h2>
+            <div class="text">Меня зовут Дмитрий, я играю на ханге. <br>Этот инструмент обладает уникальным звучанием, <br>не похожим ни на что иное и способным заполнить<br>большое пространство.</div>
+            <a target="blank" href="https://youtu.be/G9_pUmt-nww" class="link-button">смотреть видео</a>
             <div class="text">
                 Короткий отрывок моей игры на ханге позволит Вам самим<br>убедиться в волшебстве его звучания. <br>Этот инструмент не требует дополнительной звукоусиливающей<br>аппаратуры. Это его чистый, живой звук.
             </div>
@@ -55,7 +58,7 @@
     <section class="gift" id="gift">
         <img src="./bg/hang-left.png" alt="" class="step-left-img">
         <div class="container">
-            <div class="title">предлагаю вам...</div>
+            <h3 class="title">предлагаю вам...</h3>
             <div class="gift-blocks">
                 <div class="gift-block">
                     <div class="text">сделать Ваше мероприятие <br>еще более запоминающимся</div>
@@ -95,12 +98,12 @@
     </section>
     <section class="contacts" id="contacts">
         <div class="container">
-            <div class="title">мои контакты</div>
+            <h3 class="title">мои контакты</h3>
             <div class="text">Для связи со мной и обсуждения возникших вопросов можете воспользоваться любым, наиболее удобным для Вас, способом:</div>
             <div class="contants-links">
-                <a target="_ blank" href="https://vk.com/sadovoy.handpan" class="contacts-link"><img src="./img/contacts/vk.png" alt="vk"></a>
-                <a target="_ blank" href="https://www.tiktok.com/@sadovoy.handpan" class="contacts-link"><img src="./img/contacts/tictok.png" alt="tictok"></a>
-                <a target="_ blank" href="https://www.youtube.com/channel/UCFAVXS2WmLdwp0NQofu1IUg" class="contacts-link"><img src="./img/contacts/youtube.png" alt="youtube"></a>
+                <a target="blank" href="https://vk.com/sadovoy.handpan" class="contacts-link"><img src="./img/contacts/vk.png" alt="vk"></a>
+                <a target="blank" href="https://www.tiktok.com/@sadovoy.handpan" class="contacts-link"><img src="./img/contacts/tictok.png" alt="tictok"></a>
+                <a target="blank" href="https://www.youtube.com/channel/UCFAVXS2WmLdwp0NQofu1IUg" class="contacts-link"><img src="./img/contacts/youtube.png" alt="youtube"></a>
             </div>
         </div> 
     </section>
@@ -119,7 +122,7 @@
         </div>
         <div class="footer-block">
             <a href="#header" class="yakor">вернуться вверх ↑</a>
-            <a href="https://vk.com/artur.safin" target="_blank">Сайт выполнен Артуром Сафиным</a>
+            <a href="https://vk.com/artur.safin" target="blank">Сайт выполнен Артуром Сафиным</a>
         </div>
     </footer>
     <div class="popup">
@@ -129,16 +132,16 @@
             <div class="popup__body_content">
                 <div class="popup__body_content-title">ЗАПИСЬ КОМПОЗИЦИИ ИЛИ ОТДЕЛЬНОЙ ПАРТИИ</div>
                 <div class="popup__body_content-text">Если Вам необходима композиция с хангом в качестве фоновой музыки для видео или если Вы хотите интегрировать данный инструмент в свою песню или бит, я могу записать её для Вас!</div>
-                <form id="popup-form" action="" method="post">
-                    <input class="popup-name" type="name" placeholder="Как к вам обращаться?" onkeypress="noDigits(event)" require>
+                <form id="popup-form">
+                    <input class="popup-name" type="text" placeholder="Как к вам обращаться?" onkeypress="noDigits(event)">
                     <script>
                         function noDigits(event) {
                         if ("1234567890".indexOf(event.key) != -1)
                             event.preventDefault();
                         }
                     </script>
-                    <input class="popup-tel" type="tel" placeholder="Ваш номер телефона" require>
-                    <input class="popup-tg" type="tg" placeholder="Ваш аккаунт телеграмма(необязательно)">
+                    <input class="popup-tel" type="tel" placeholder="Ваш номер телефона">
+                    <input class="popup-tg" type="text" placeholder="Ваш аккаунт телеграмма(необязательно)">
                 </form>
                 <button class="popup__body_content-button">связаться</button>
                 <div class="error"></div>
